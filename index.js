6c870bf792c42198a5aef428a9dd1fede2cf0855
Nishant <nishant.dwivedi@hotmail.com>
2023-07-23 15:12:38 +0530

    final commit

--- index.js
+++ index.js
@@ -3,35 +3,36 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const Person = require("./mongo")
-const { default: mongoose } = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 
-
-// json parser for POST
+// json parser middleware for parsing POST requests and populating the req object with the payload
 app.use("/api/persons",express.json());
-// cors, static frontend
+// cors and static frontend middlewares
 app.use(cors(), express.static("build"));
-
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
 
+// GET routes
 app.get("/api/persons", (req, res) => {
   Person.find({}).then(data => {
     res.json(data)
+  }).catch(err => {
+    next(err);
   })
-
 });
 
 app.get("/api/info", (req, res) => {
   let persons;
   Person.find({}).then(data =>{
     persons = data
-    res.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`);
+    res.send(`Phonebook directory has info for ${persons.length} people <br> ${new Date()}`);
+  }).catch(err => {
+    next(err)
   })
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
   Person.findById({_id : id}).then(result =>{
     if(result){
@@ -41,73 +42,71 @@ app.get("/api/persons/:id", (req, res) => {
       res.status(404).end();
     }
   }).catch(err =>{
-    console.log(err);
-    res.status(400).send({error: "malformed id"})
+    next(err);
   })
 });
-// DELETE
+
+// DELETE route 
 app.delete("/api/persons/:name", (req, res) => {
   const name = req.params.name;
-  
-console.log(`request to delete: ${name}`);
+  console.log(`request to delete: ${name}`);
   Person.deleteOne({name: name}).then(response => {
     console.log(response);
     res.status(204).end();
   }).catch(err => {
-    console.log(err);
+    console.log(err.name);
     res.status(500).end();
   })
-  
 });
-// POST
+
+// POST route
 app.post("/api/persons", (req, res, next) => {
-  console.log(req.body);
+  console.log("POST request received");
   if (req.body.name == undefined || req.body.number == undefined) {
+    console.log("invalid format used in request body.");
     res
       .status(400)
-      .send("<p>Invalid format used for state's representation.</p>");
+      .send("<p>Invalid format used in request body.</p>");
   } 
   else {
    const newPerson = new Person({
     name: req.body.name,
     number: req.body.number
    })
-   newPerson.save().then(response => {
-    console.log(response);
+   newPerson.save()
+   .then(response => {
+    console.log("saved new entry to database", response);
     res.status(201).send(response);
    }).catch(err=>{
-    console.log(err.message);
     next(err);
-    // res.status(500).end()
   })
   }
 });
-
-// PUT
-app.put("/api/persons/:id" , (req,res) => {
-  console.log(req.body);
-  if(typeof(req.body.number) != "string"){
-    console.log("invalid number type provided");
-    res.status(400).send({error: "provide a valid phone number"})
-  }
-  else{
+// PUT route
+app.put("/api/persons/:id" , (req,res,next) => {
+    console.log("PUT request received");
     Person.updateOne({_id: req.body._id}, {number:req.body.number}).then(respose => {
       console.log(respose);
       res.status(200).send(respose);
     }).catch(err => {
-      console.log(err);
-      res.status(500).end();
+      next(err)
     })
-  }
 })
-
 // error handling
 function errorHandler (err, req, res, next){
+  // validation error
   if(err.name === 'ValidationError'){
     console.log(err.message);
     return res.status(500).json({error: err.message});
   }
-  next(err);
+  // cast error
+  else if(err.name === "CastError"){
+    console.log(err.message);
+    res.status(400).json({error: err.message})
+  }
+  // other errors
+    console.log(err.message);
+    res.status(500).send({error: "internal server error"})
 }
 
 app.use(errorHandler);
--- mongo.js
+++ mongo.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery',false);
-
+// get url from the hosting environment
 const url = process.env.URL;
-
-console.log(url);
+// create the schema and transform it as required
 const personsSchema = new mongoose.Schema({
     name:{
         type: String,
@@ -12,21 +11,20 @@ const personsSchema = new mongoose.Schema({
     },
     number: Number,
 });
-
-personsSchema.set('toObject', {transform:function (doc, ret, options){
+personsSchema.set('toObject', {transform:function (doc, ret){
     ret.id = ret._id.toString();
     delete ret.__v;
     delete ret._id;
     return ret;
 }})
-
+// connect to database
 console.log("connecting to database");
 mongoose.connect(url).then(res => {
     console.log("connected to database");
 }).catch(error => {
     console.log(error);
 })
-
+// export the model for creating instances of the schema and storing in db
 module.exports= mongoose.model('Person', personsSchema);
 
 
